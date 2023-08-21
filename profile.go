c69cca1ccfc9e3f8912287728a361ae0893b2f8f
Sümer Cip <sumer.cip@platform.sh>
2023-08-21 11:12:45 +0300

    fix profiletype string method

--- profile.go
+++ profile.go
@@ -16,6 +16,10 @@ func (t ProfileType) String() string {
 	switch t {
 	case CPUProfile:
 		return "cpu"
+	case HeapProfile:
+		return "heap"
+	case GoroutineProfile:
+		return "goroutine"
 	default:
 		return fmt.Sprintf("invalid profile type (%d)", int(t))
 	}
