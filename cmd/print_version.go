0c950b6da5228ed3fbe8b3d92faf672d3818d640
Julien Jean <julien.jean@platform.sh>
2024-01-22 15:58:40 +0100

    remove last reference to experimental

--- cmd/print_version.go
+++ cmd/print_version.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	profiler "github.com/blackfireio/go-continuous-profiling-experimental"
+	profiler "github.com/blackfireio/go-continuous-profiling"
 )
 
 func main() {
